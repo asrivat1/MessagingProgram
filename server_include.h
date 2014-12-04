a9f2d262180ece03b7af3ad137d26dcee88ba0bd
trevoraron <taron1@jhu.edu>
2014-12-03 22:01:48 -0500

    Changed serv_msg

--- server_include.h
+++ server_include.h
@@ -9,11 +9,12 @@ typedef struct lts{
 /*Server msg struct */
 typedef struct serv_msg{
     lts stamp;
-    short is_merge;
     short type;
-    int payload_len;
+    char user[10];
+    char room[30];
+    char payload[80];
     char * payload;
     /*You can not hold strings in a struct, so payload has to be malloced.
      *When a msg is sent, payload needs to be sent too. */
-}
+}serv_msg;
 #endif /* SERVER_INCLUDE */
